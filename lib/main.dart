51661bcff2d51e894d30be6317a245ff73e57aa0
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-20 23:33:43 -0400

    inicio de proyecto

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/src/pages/MusicPlayerPage.dart';
+import 'package:music_player/src/theme/theme.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music Players',
+      theme: miTema,
+      home: MusicPlayerPage()
+    );
+  }
+}
