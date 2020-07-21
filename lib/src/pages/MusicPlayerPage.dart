4c1006960b94979d414f0a41c56cb2cff9fb8dee
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-21 00:10:57 -0400

    imagen de disco

--- lib/src/pages/MusicPlayerPage.dart
+++ lib/src/pages/MusicPlayerPage.dart
@@ -5,7 +5,60 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomAppBar(),
+      body: Column(
+        children: [
+          CustomAppBar(),
+          ImagenDiscoDuracion()
+        ],
+      ),
+    );
+  }
+}
+
+class ImagenDiscoDuracion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      width: 250.0,
+      height: 250.0,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(200.0),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+
+            Image(image: AssetImage("assets/aurora.jpg")),
+
+            Container(
+              width: 25.0,
+              height: 25.0,
+              decoration: BoxDecoration(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(100)
+              ),
+            ),
+            Container(
+              width: 18.0,
+              height: 18.0,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(100)
+              ),
+            )
+          ],
+        )
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200.0),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Color(0xff484750),
+            Color(0xff1E1C24),
+          ]
+        )
+      ),
     );
   }
 }
